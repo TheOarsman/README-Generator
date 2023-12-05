31e3955dbf65dfd5374ebe0332c9ee8b7ef26e7d
Heinz Ulrich <heinzulrichv@gmail.com>
2023-12-05 15:13:38 -0500

    license list created

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ inquirer
         {
             type: 'input',
             name: 'webAddress',
-            message: 'What is the web address to your site? (copy/paste URL)',
+            message: 'Copy/Paste the URL to your site',
         },
         {
             type: 'input',
@@ -200,9 +200,10 @@ inquirer
             message: `Copy/Paste the URL to the GitHub Profile of who you collaborated with`,
         },
         {
-            type: `input`,
+            type: `list`,
             name: `license`,
-            message: `What kind of licensing do you want applied to your project?`,
+            message: `What kind of licensing do you want applied to your project? NOTE: Use the Up/Down arrows to navigate through the list, then press "Enter" to select your choice.`,
+            choices: [`AFL-3.0`, `Apache-2.0`, `Artistic-2.0`, `BSL-1.0`, `BSD-2-Clause`, `BSD-3-Clause`, `BSD-3-Clause-Clear`, `BSD-4-Clause`, `0BSD`, `CC`, `CC0-1.0`, `CC-BY-4.0`, `CC-BY-SA-4.0`, `WTFPL`, `ECL-2.0`, `EPL-1.0`, `EPL-2.0`, `EUPL-1.1`, `AGPL-3.0`, `GPL`, `GPL-2.0`, `GPL-3.0`, `LGPL`, `LGPL-2.1`, `LGPL-3.0`, `ISC`, `LPPL-1.3c`, `MS-PL`, `MIT`, `MPL-2.0`, `OSL-3.0`, `PostgreSQL`, `OFL-1.1`, `NCSA`, `Unlicense`, `Zlib`],
         },
         {
             type: `input`,
