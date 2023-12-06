4765bb657e2316bb15707e8deca64d886b3b4cd9
Heinz Ulrich <heinzulrichv@gmail.com>
2023-12-06 18:34:00 -0500

    Changing projected to Markdown, not HTML

--- index.js
+++ index.js
@@ -1,89 +1,209 @@
-function init() { 
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generate = require('./utils/generateMarkdown.js');
 const path = require('path');
 
-const generateHTML = ({ title, description, webAddress, img, as, iWant, soThat, given, when, then, steps, instructions, collabName, collabGitHub, collabGitHubURL, first, last, license, tests, question, resourceName, resourceURL, userName, gitURL, linkedIn, linkedInName, eMail, licenseBadge }) =>
-    `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-  <title>README</title>
-</head>
-<body>
-  <header class="p-5 mb-4 header bg-light">
-    <div class="container">
-      <h1 class="display-4">${title}</h1>
+function init() {
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'title',
+                message: 'What is the title of your Web Page?',
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Give a brief description of your REPO. Ideas for description: What was your motivation? Why did you build this REPO? What problem(s), if any, did it solve? What did you learn?',
+            },
+            {
+                type: 'input',
+                name: 'webAddress',
+                message: 'Copy/Paste the URL to your site',
+            },
+            {
+                type: 'input',
+                name: 'img',
+                message: 'Paste an image of your web page.',
+            },
+            {
+                type: 'input',
+                name: 'as',
+                message: 'Creating your User Story: What is your User Story? [1 - AS..., 2 - I WANT..., 3 - SO THAT...] Part 1 of 3: Finish the "AS" statement (DO NOT USE THE WORD "AS" AT THE BEGINING OF YOUR STATEMENT)',
+            },
+            {
+                type: 'input',
+                name: 'iWant',
+                message: 'Creating your User Story: What is your User Story? [1 - AS..., 2 - I WANT..., 3 - SO THAT...] Part 2 of 3: Finish the "I WANT" statement (DO NOT USE THE WORDS "I WANT" AT THE BEGINING OF YOUR STATEMENT)',
+            },
+            {
+                type: 'input',
+                name: 'soThat',
+                message: 'Creating your User Story: What is your User Story? [1 - AS..., 2 - I WANT..., 3 - SO THAT...] Part 3 of 3: Finish the "SO THAT" statement (DO NOT USE THE WORDS "SO THAT" AT THE BEGINING OF YOUR STATEMENT)',
+            },
+            {
+                type: 'input',
+                name: 'given',
+                message: 'What is the Acceptance Criteria? [Given..., When..., Then...] Part 1 of 3: Finish the "GIVEN" statement (DO NOT USE THE WORD "GIVEN" AT THE BEGINING OF YOUR STATEMENT)',
+            },
+            {
+                type: 'input',
+                name: 'when',
+                message: 'What is the Acceptance Criteria? [Given..., When..., Then...] Part 2 of 3: Finish the "WHEN" statement (DO NOT USE THE WORD "WHEN" AT THE BEGINING OF YOUR STATEMENT)',
+            },
+            {
+                type: 'input',
+                name: 'then',
+                message: 'What is the Acceptance Criteria? [Given..., When..., Then...] Part 3 of 3: Finish the "THEN" statement (DO NOT USE THE WORD "THEN" AT THE BEGINING OF YOUR STATEMENT)',
+            },
+            {
+                type: 'input',
+                name: 'steps',
+                message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
+            },
+            {
+                type: 'input',
+                name: 'instructions',
+                message: 'Provide instructions and examples for use. Include screenshots and/or "code example(s)" as needed.',
+            },
+            {
+                type: 'input',
+                name: 'collabName',
+                message: 'List the name of who you collaborated with. If none, enter "N/A"',
+            },
+            {
+                type: `input`,
+                name: `collabGitHub`,
+                message: `List the GitHub Profile Name of who you collaborated with. If none, enter "N/A"`,
+            },
+            {
+                type: `input`,
+                name: `collabGitHubURL`,
+                message: `Copy/Paste the URL to the GitHub Profile of who you collaborated with`,
+            },
+            {
+                type: `input`,
+                name: `first`,
+                message: `Enter your First name as you would like displayed for copyright/licensing`,
+            },
+            {
+                type: `input`,
+                name: `last`,
+                message: `Enter your Last name as you would like displayed for copyright/licensing`,
+            },
+            {
+                type: `rawlist`,
+                name: `license`,
+                message: `What kind of licensing do you want applied to your project? NOTE: Use the Up/Down arrows to navigate through the list, type the number option then press "Enter", or highlight your selection and press "Enter", to select your choice.`,
+                choices: [`Apache 2.0 License`, `BSD 3-Clause License`, `BSD 2-Clause License`, `Attribution 4.0 International`, `Attribution-ShareAlike 4.0 International`, `Attribution-NonCommercial 4.0 International`, `Attribution-NoDerivates 4.0 International`, `Attribution-NonCommmercial-ShareAlike 4.0 International`, `Attribution-NonCommercial-NoDerivatives 4.0 International`, `Eclipse Public License 1.0`, `GNU GPL v3`, `GNU GPL v2`, `GNU AGPL v3`, `GNU LGPL v3`, `GNU FDL v1.3`, `IBM Public License Version 1.0`, `The MIT License`, `Mozilla Public License 2.0`, `Attribution License (BY)`, `Open Database License (ODbL)`, `Public Domain Dedication and License (PDDL)`, `The Perl License`, `The Artistic License 2.0`, `The zlib/libpng License`],
+            },
+            {
+                type: `input`,
+                name: `tests`,
+                message: `What tests do you have for this application?`,
+            },
+            {
+                type: `input`,
+                name: `question`,
+                message: `What quetsions are there to ask?`,
+            },
+            {
+                type: `input`,
+                name: `resourceName`,
+                message: `What resource(s) did you use to create your project? (Insert Name)`,
+            },
+            {
+                type: `input`,
+                name: `resourceURL`,
+                message: `Copy/Paste URL of the resource used`,
+            },
+            {
+                type: 'input',
+                name: 'userName',
+                message: 'Enter your GitHub Username.',
+            },
+            {
+                type: 'input',
+                name: 'gitURL',
+                message: 'Copy/Paste your GitHub Profile URL.',
+            },
+            {
+                type: 'input',
+                name: 'linkedIn',
+                message: 'Copy/Paste your LinkedIn URL.',
+            },
+            {
+                type: `input`,
+                name: `linkedInName`,
+                message: `What is your display name on LinkedIn?`
+            },
+            {
+                type: 'input',
+                name: 'eMail',
+                message: 'Enter your E-Mail address.',
+            },
+        ])
+        .then((answers) => {
+            const markdownContent = generateREADME(answers);
 
-      <br>
-    
-      <h3>DESCRIPTION</span></h3>
-      <p class="lead">${description}.</p>
-      <h6 class="webAddress"><a href="${webAddress}">${webAddress}</a></h6>
+            fs.writeFile('example.md', markdownContent, (err) =>
+                err ? console.log(err) : console.log('Successfully created example.md')
+            );
+        });
+}
+// TODO: Create a function to write README file
+// function writeToFile(fileName, data) { }
+const generateREADME = ({ title, description, webAddress, img, as, iWant, soThat, given, when, then, steps, instructions, collabName, collabGitHub, collabGitHubURL, first, last, license, tests, question, resourceName, resourceURL, userName, gitURL, linkedIn, linkedInName, eMail, licenseBadge }) =>
+`
+# ${title}
 
-      <br>
+### DESCRIPTION
 
-      <img class="container">${img}</img>
+${description}
 
-      <br>
+####### [${webAddress}](${webAddress})
 
-      <h3>TABLE OF CONTENTS</span></h3>
-      <ul class="list-group">
-        <li class="list-group-item"><a href="#userStory">USER STORY</li>
-        <li class="list-group-item"><a href="#acceptCrit">ACEEPTANCE CRITERIA</li>
-        <li class="list-group-item"><a href="#installation">INSTALLATION</li>
-        <li class="list-group-item"><a href="#usage">USAGE</a></li>
-        <li class="list-group-item"><a href="#license">LICENSE</li>  
-        <li class="list-group-item"><a href="#contributing">CONTRIBUTING</li> 
-        <li class="list-group-item"><a href="#tests">TESTS</li>
-        <li class="list-group-item"><a href="#questions">QUESTIONS</li>
-        <li class="list-group-item"><a href="#resources">RESOURCES</li>
-        <li class="list-group-item"><a href="#contactMe">CONTACT ME</li>
-      </ul>
+<img class="container">${img}</img>
 
-      <br>
+### TABLE OF CONTENTS
 
-      <h3><a id="userStory">USER STORY</h3>
-      <ul class="list-group">
-        <li class="list-group-item"><b>AS</b> ${as}</li>
-        <li class="list-group-item"><b>I WANT</b> ${iWant}</li>
-        <li class="list-group-item"><b>SO THAT</b> ${soThat}</li>
-      </ul>
+<a href="#userStory">USER STORY</li>
+<a href="#acceptCrit">ACEEPTANCE CRITERIA</li>
+<a href="#installation">INSTALLATION</li>
+<a href="#usage">USAGE</a></li>
+<a href="#license">LICENSE</li>  
+<a href="#contributing">CONTRIBUTING</li> 
+<a href="#tests">TESTS</li>
+<a href="#questions">QUESTIONS</li>
+<a href="#resources">RESOURCES</li>
+<a href="#contactMe">CONTACT ME</li>
+
+### USER STORY
+   
+  AS</b> ${as}</li>
+  I WANT</b> ${iWant}</li>
+  SO THAT</b> ${soThat}</li>
+
+
+### ACCEPTANCE CRITERIA</h3>
+
+  GIVEN</b> ${given}</li>
+  WHEN</b> ${when}</li>
+  THEN</b> ${then}</li>
+
+### INTALLATION
       
-      <br>
+${steps}
 
-      <h3><a id="acceptCrit">ACCEPTANCE CRITERIA</h3>
-      <ul class="list-group">
-        <li class="list-group-item"><b>GIVEN</b> ${given}</li>
-        <li class="list-group-item"><b>WHEN</b> ${when}</li>
-        <li class="list-group-item"><b>THEN</b> ${then}</li>
-      </ul>
+### USAGE
 
-      <br>
+${instructions}
+   
+### CONTRIBUTING</h3>
 
-      <h3><a id="installation">INTALLATION</h3>
-      <ul class="list-group">
-        <li class="list-group-item">${steps}</li>
-      </ul>
-
-      <br>
-
-      <h3><a id="usage">USAGE</a></h3>
-      <ul class="list-group">
-        <li class="list-group-item">${instructions}</li>
-      </ul>
-
-      <br>
-
-      <h3><a id="contributing">CONTRIBUTING</h3>
-      <ul class="list-group">
-        <li class="list-group-item">Collaborator Name: ${collabName}</li>
-        <li class="list-group-item">Collaborator GitHub: ${collabGitHub}: <a href="${collabGitHubURL}"></li>
-      </ul>
+Collaborator Name: ${collabName}</li>
+Collaborator GitHub: ${collabGitHub}: <a href="${collabGitHubURL}"></li>
+ 
 
       <br>
 
@@ -126,156 +246,4 @@ const generateHTML = ({ title, description, webAddress, img, as, iWant, soThat, 
 </body>
 </html>`;
 
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your Web Page?',
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Give a brief description of your REPO. Ideas for description: What was your motivation? Why did you build this REPO? What problem(s), if any, did it solve? What did you learn?',
-        },
-        {
-            type: 'input',
-            name: 'webAddress',
-            message: 'Copy/Paste the URL to your site',
-        },
-        {
-            type: 'input',
-            name: 'img',
-            message: 'Paste an image of your web page.',
-        },
-        {
-            type: 'input',
-            name: 'as',
-            message: 'Creating your User Story: What is your User Story? [1 - AS..., 2 - I WANT..., 3 - SO THAT...] Part 1 of 3: Finish the "AS" statement (DO NOT USE THE WORD "AS" AT THE BEGINING OF YOUR STATEMENT)',
-        },
-        {
-            type: 'input',
-            name: 'iWant',
-            message: 'Creating your User Story: What is your User Story? [1 - AS..., 2 - I WANT..., 3 - SO THAT...] Part 2 of 3: Finish the "I WANT" statement (DO NOT USE THE WORDS "I WANT" AT THE BEGINING OF YOUR STATEMENT)',
-        },
-        {
-            type: 'input',
-            name: 'soThat',
-            message: 'Creating your User Story: What is your User Story? [1 - AS..., 2 - I WANT..., 3 - SO THAT...] Part 3 of 3: Finish the "SO THAT" statement (DO NOT USE THE WORDS "SO THAT" AT THE BEGINING OF YOUR STATEMENT)',
-        },
-        {
-            type: 'input',
-            name: 'given',
-            message: 'What is the Acceptance Criteria? [Given..., When..., Then...] Part 1 of 3: Finish the "GIVEN" statement (DO NOT USE THE WORD "GIVEN" AT THE BEGINING OF YOUR STATEMENT)',
-        },
-        {
-            type: 'input',
-            name: 'when',
-            message: 'What is the Acceptance Criteria? [Given..., When..., Then...] Part 2 of 3: Finish the "WHEN" statement (DO NOT USE THE WORD "WHEN" AT THE BEGINING OF YOUR STATEMENT)',
-        },
-        {
-            type: 'input',
-            name: 'then',
-            message: 'What is the Acceptance Criteria? [Given..., When..., Then...] Part 3 of 3: Finish the "THEN" statement (DO NOT USE THE WORD "THEN" AT THE BEGINING OF YOUR STATEMENT)',
-        },
-        {
-            type: 'input',
-            name: 'steps',
-            message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.',
-        },
-        {
-            type: 'input',
-            name: 'instructions',
-            message: 'Provide instructions and examples for use. Include screenshots and/or "code example(s)" as needed.',
-        },
-        {
-            type: 'input',
-            name: 'collabName',
-            message: 'List the name of who you collaborated with. If none, enter "N/A"',
-        },
-        {
-            type: `input`,
-            name: `collabGitHub`,
-            message: `List the GitHub Profile Name of who you collaborated with. If none, enter "N/A"`,
-        },
-        {
-            type: `input`,
-            name: `collabGitHubURL`,
-            message: `Copy/Paste the URL to the GitHub Profile of who you collaborated with`,
-        },
-        {
-            type: `input`,
-            name: `first`,
-            message: `Enter your First name as you would like displayed for copyright/licensing`,
-        },
-        {
-            type: `input`,
-            name: `last`,
-            message: `Enter your Last name as you would like displayed for copyright/licensing`,
-        },
-        {
-            type: `rawlist`,
-            name: `license`,
-            message: `What kind of licensing do you want applied to your project? NOTE: Use the Up/Down arrows to navigate through the list, type the number option then press "Enter", or highlight your selection and press "Enter", to select your choice.`,
-            choices: [`Apache 2.0 License`, `BSD 3-Clause License`, `BSD 2-Clause License`, `Attribution 4.0 International`, `Attribution-ShareAlike 4.0 International`, `Attribution-NonCommercial 4.0 International`, `Attribution-NoDerivates 4.0 International`, `Attribution-NonCommmercial-ShareAlike 4.0 International`, `Attribution-NonCommercial-NoDerivatives 4.0 International`, `Eclipse Public License 1.0`, `GNU GPL v3`, `GNU GPL v2`, `GNU AGPL v3`, `GNU LGPL v3`, `GNU FDL v1.3`, `IBM Public License Version 1.0`, `The MIT License`, `Mozilla Public License 2.0`, `Attribution License (BY)`, `Open Database License (ODbL)`, `Public Domain Dedication and License (PDDL)`, `The Perl License`, `The Artistic License 2.0`, `The zlib/libpng License`],
-        },
-        {
-            type: `input`,
-            name: `tests`,
-            message: `What tests do you have for this application?`,
-        },
-        {
-            type: `input`,
-            name: `question`,
-            message: `What quetsions are there to ask?`,
-        },
-        {
-            type: `input`,
-            name: `resourceName`,
-            message: `What resource(s) did you use to create your project? (Insert Name)`,
-        },
-        {
-            type: `input`,
-            name: `resourceURL`,
-            message: `Copy/Paste URL of the resource used`,
-        },
-        {
-            type: 'input',
-            name: 'userName',
-            message: 'Enter your GitHub Username.',
-        },
-        {
-            type: 'input',
-            name: 'gitURL',
-            message: 'Copy/Paste your GitHub Profile URL.',
-        },
-        {
-            type: 'input',
-            name: 'linkedIn',
-            message: 'Copy/Paste your LinkedIn URL.',
-        },
-        {
-            type: `input`,
-            name: `linkedInName`,
-            message: `What is your display name on LinkedIn?`
-        },
-        {
-            type: 'input',
-            name: 'eMail',
-            message: 'Enter your E-Mail address.',
-        },
-    ])
-    .then((answers) => {
-        const htmlPageContent = generateHTML(answers);
-
-        fs.writeFile('index.html', htmlPageContent, (err) =>
-            err ? console.log(err) : console.log('Successfully created index.html!')
-        );
-    });
-}
-
-// Function call to initialize app
 init();
-
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) { }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,41 +4,8 @@
 function licenseBadge(data) {
   const licenseType = data.license[0];
   let licenseString = " "
-  //if (licenseType === "Apache 2.0 License") {
-  //  licenseString = `![License: MIT](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
-  //};
-  //if (licenseType === "BSD 3-Clause License") {
-  //  licenseString = `![License: GPL v3](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`
-  //};
-  //if (licenseType === "BSD 2-Clause License") {
-  //  licenseString = `![License: GPL v3](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)`
-  //};
-  if (licenseType === "Attribution 4.0 International") {
-    licenseString = `![License: CC BY 4.0](https://img.shields.io/badge/License-CC%20BY%204.0-lightgrey.svg)`
-  };
-  if (licenseType === "Attribution-ShareAlike 4.0 International") {
-    licenseString = `![License: CC BY-SA 4.0](https://img.shields.io/badge/License-CC%20BY--SA%204.0-lightgrey.svg)`
-  };
-  if (licenseType === "Attribution-NonCommercial 4.0 International") {
-    licenseString = `![License: CC BY-NC 4.0](https://img.shields.io/badge/License-CC%20BY--NC%204.0-lightgrey.svg)`
-  };
-  if (licenseType === "Attribution-NoDerivates 4.0 International") {
-    licenseString = `![License: CC BY-ND 4.0](https://img.shields.io/badge/License-CC%20BY--ND%204.0-lightgrey.svg)`
-  };
-  if (licenseType === "Attribution-NonCommmercial-ShareAlike 4.0 International") {
-    licenseString = `![License: CC BY-NC-SA 4.0](https://img.shields.io/badge/License-CC%20BY--NC--SA%204.0-lightgrey.svg)`
-  };
-  if (licenseType === "Attribution-NonCommercial-NoDerivatives 4.0 International") {
-    licenseString = `![License: CC BY-NC-ND 4.0](https://img.shields.io/badge/License-CC%20BY--NC--ND%204.0-lightgrey.svg)`
-  };
-  //if (licenseType === "Eclipse Public License 1.0") {
-  //  licenseString = `![License:    ](https://img.shields.io/badge/License-EPL%201.0-red.svg)`
-  //};
-  if (licenseType === "GNU GPL v3") {
-    licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)`
-  };
-  if (licenseType === "GNU GPL v2") {
-    licenseString = `![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)`
+  if (licenseType === "Apache 2.0 License") {
+    licenseString = `![License: MIT](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
   };
   if (licenseType === "GNU AGPL v3") {
     licenseString = `![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)`
@@ -46,35 +13,8 @@ function licenseBadge(data) {
   if (licenseType === "GNU LGPL v3") {
     licenseString = `![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)`
   };
-  if (licenseType === "GNU FDL v1.3") {
-    licenseString = `![License: FDL 1.3](https://img.shields.io/badge/License-FDL%20v1.3-blue.svg)`
-  };
-  if (licenseType === "IBM Public License Version 1.0") {
-    licenseString = `![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)`
-  };
-  if (licenseType === "The MIT License") {
-    licenseString = `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
-  };
   if (licenseType === "Mozilla Public License 2.0") {
     licenseString = `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`
-  };
-  if (licenseType === "Attribution License (BY)") {
-    licenseString = `![License: Open Data Commons Attribution](https://img.shields.io/badge/License-ODC_BY-brightgreen.svg)`
-  };
-  if (licenseType === "Open Database License (ODbL)") {
-    licenseString = `![License: ODbL](https://img.shields.io/badge/License-ODbL-brightgreen.svg)`
-  };
-  if (licenseType === "Public Domain Dedication and License (PDDL)") {
-    licenseString = `![License: ODbL](https://img.shields.io/badge/License-PDDL-brightgreen.svg)`
-  };
-  if (licenseType === "The Perl License") {
-    licenseString = `![License: Artistic-2.0](https://img.shields.io/badge/License-Perl-0298c3.svg)`
-  };
-  if (licenseType === "The Artistic License 2.0") {
-    licenseString = `![License: Artistic-2.0](https://img.shields.io/badge/License-Artistic%202.0-0298c3.svg)`
-  };
-  if (licenseType === "The zlib/libpng License") {
-    licenseString = `![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)`
   };
   return licenseString
 };
