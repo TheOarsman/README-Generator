da58c77fd3bf7db318743704d4749b558d898cb1
Heinz Ulrich <heinzulrichv@gmail.com>
2023-12-10 22:58:48 -0500

    links added for licensing

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,34 +3,34 @@ function licenseBadge(data) {
   const liceType = data.liceChoice;
   let licenseString = " "
   if (liceType === "GNU AGPLv3") {
-    licenseString = `![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)`
+    licenseString = `![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)
+    
+    https://choosealicense.com/licenses/agpl-3.0/`
   };
   if (liceType === "GNU GPLv3") {
-    licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)`
+    licenseString = `![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)
+    
+    https://choosealicense.com/licenses/gpl-3.0/`
   };
   if (liceType === "GNU LGPLv3") {
-    licenseString = `![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)`
+    licenseString = `![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)
+    
+    https://choosealicense.com/licenses/lgpl-3.0/`
   };
   if (liceType === "Mozilla Public License 2.0") {
-    licenseString = `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)`
+    licenseString = `![License: MPL 2.0](https://img.shields.io/badge/License-MPL%202.0-brightgreen.svg)
+    
+    https://choosealicense.com/licenses/mpl-2.0/`
   };
   if (liceType === "Apache License 2.0") {
     licenseString = `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.`
+    
+    https://choosealicense.com/licenses/apache-2.0/`
   };
   if (liceType === "MIT License") {
-    licenseString = `![License: MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)`
+    licenseString = `![License: MIT License](https://img.shields.io/badge/License-MIT-yellow.svg)
+    
+    https://choosealicense.com/licenses/mit/`
   };
   return licenseString
 };
@@ -94,13 +94,7 @@ GitHub: [${data.collabGitHub}](${data.collabGitHubURL})
 
 Copyright (c) ${data.first} ${data.last}
 
-GitHub: [${data.userName}](${data.gitURL})
-
-Licensed under ${data.liceChoice}
-
 ${licenseBadge(data)}
-
-
 
 ## TESTS
 
